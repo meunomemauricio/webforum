55fa24e22ed5356a7365f2af3270071be5ac8d93
Mauricio Freitas <contato@mauriciofreitas.eng.br>
2016-10-13 23:25:29 -0300

    Renamed Logout controller class

--- src/controllers/LogoutController.java
+++ src/controllers/LogoutController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/logout")
-public class Logout extends HttpServlet {
+public class LogoutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
